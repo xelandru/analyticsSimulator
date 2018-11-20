f8473c4c556f70b62a8c7ff4b6f31c84ff873419
adascalu <alexandru.dascalu@adswizz.com>
2018-11-20 22:02:57 +0200

    using apache io to read file

--- src/main/java/EventProducer.java
+++ src/main/java/EventProducer.java
@@ -1,11 +1,6 @@
 import org.apache.commons.io.input.Tailer;
 import org.apache.commons.io.input.TailerListenerAdapter;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.BlockingQueue;
@@ -15,14 +10,31 @@ public class EventProducer implements Runnable {
     private final BlockingQueue<String> eventQueue;
     private final String pathToEventsFile;
 
-
-
     public EventProducer(BlockingQueue<String> eventQue, String pathToSource) {
         this.eventQueue = eventQue;
         this.pathToEventsFile = pathToSource;
     }
 
-//    @Override
+    @Override
+    public void run() {
+        Path pathToFile = Paths.get(pathToEventsFile).toAbsolutePath();
+        MyTailerListener listener = new MyTailerListener();
+        Tailer tailer = new Tailer(pathToFile.toFile(), listener);
+        tailer.run();
+    }
+
+    private class MyTailerListener extends TailerListenerAdapter {
+        public void handle(String line) {
+            if (line != null && !(line.isEmpty())) {
+                try {
+                    eventQueue.put(line);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    //    @Override
 //    public void run() {
 //        Path pathToFile = Paths.get(pathToEventsFile).toAbsolutePath();
 //        String line;
@@ -43,27 +55,4 @@ public class EventProducer implements Runnable {
 //            Thread.currentThread().interrupt();
 //        }
 //    }
-
-    @Override
-    public void run() {
-        Path pathToFile = Paths.get(pathToEventsFile).toAbsolutePath();
-
-        MyTailerListener listener = new MyTailerListener();
-        Tailer tailer = new Tailer(pathToFile.toFile(), listener, 0L);
-
-        tailer.run();
-
-    }
-
-    private  class MyTailerListener extends TailerListenerAdapter {
-        public void handle(String line) {
-            if (line != null && !(line.isEmpty())) {
-                try {
-                    eventQueue.put(line);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
